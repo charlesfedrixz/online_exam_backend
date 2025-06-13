f29ac54797452558f73dc2149a3805c0d75f308c
charlesfedrixz <chandtekcham@gmail.com>
2025-06-13 14:44:36 +0530

    updated mongodb issue

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose
-    .connect(process.env.MONGODB_URL)
-    .then((result) => {
-      console.log("Connected to MongoDB");
-      // Start your server or perform other operations
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB:", error);
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // Custom timeout for faster failure
     });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1); // Exit the app if DB connection fails
+  }
 };
+
 module.exports = connectDB;
