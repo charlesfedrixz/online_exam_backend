27373c78fd7dd5e696444dfc2833d947ecafe138
charlesfedrixz <chandtekcham@gmail.com>
2025-06-13 18:31:04 +0530

    updated mongodb

--- server.js
+++ server.js
@@ -41,13 +41,10 @@ connectWithRetry();
 
 // CORS configuration
 const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? [process.env.PRODUCTION_ORIGIN]
-      : [
-          process.env.PRODUCTION_ORIGIN,
-          process.env.DEVELOPMENT_ORIGIN || "http://localhost:5173",
-        ],
+  origin: [
+    process.env.PRODUCTION_ORIGIN,
+    process.env.DEVELOPMENT_ORIGIN || "http://localhost:5173",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: [
